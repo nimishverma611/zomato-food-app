1de7cc8e40946aee4eb4667801212154d7efb3e7
nimishverma611 <149095244+nimishverma611@users.noreply.github.com>
2026-02-04 15:15:23 +0530

    Reorder middleware usage in app.js

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json());
 app.use(express.static("public"));
+app.use(express.json());
 
-// Sample food data
 const foods = [
   { id: 1, name: "Pizza", price: 199, image: "https://i.imgur.com/eVm7m1O.png" },
   { id: 2, name: "Burger", price: 99, image: "https://i.imgur.com/5Aqgz7o.png" },
@@ -16,17 +14,14 @@ const foods = [
   { id: 4, name: "Pasta", price: 179, image: "https://i.imgur.com/dzGJZ5v.png" }
 ];
 
-// API route
 app.get("/api/foods", (req, res) => {
   res.json(foods);
 });
 
-// Root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
