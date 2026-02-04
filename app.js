85cc59b65840e0a90e3207032d69d61df38ce565
nimishverma611 <149095244+nimishverma611@users.noreply.github.com>
2026-02-04 15:04:58 +0530

    Set up basic Express server with a root route

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Zomato Style Food App is Live 🍔🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
